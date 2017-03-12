6cd6557d2242bb96f8f17068cffc2da6d93bb178
LenaAndreichenko <eandrejchenko@gmail.com>
2017-03-12 22:26:47 +0200

    Добавлены переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,11 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String someText = "world";
+        System.out.println("Hello, " + someText + "!");
+
+        double l = 8.0;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 }
